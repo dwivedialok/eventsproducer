21244354b863cef82b7a667aad122af0ff927d2b
Alok Dwivedi <alok.dwivedi1@maersk.com>
2020-06-12 21:17:37 +0100

    basic MQ producer

--- src/main/java/eventsproducer/EventsController.java
+++ src/main/java/eventsproducer/EventsController.java
@@ -3,19 +3,26 @@ package eventsproducer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.ibm.jms.JMSBytesMessage;
+import com.ibm.msg.client.wmq.WMQConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFutureCallback;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.jms.JMSException;
+import javax.jms.Session;
+import java.io.UnsupportedEncodingException;
+import java.util.Optional;
 
 @RestController
 @EnableJms
@@ -44,10 +51,33 @@ public class EventsController {
     }
 
     @PostMapping(path = "/mqmessage/{destinationName}", consumes = "application/json", produces = "application/json" )
-    public ResponseEntity<String> postMQMessage(@RequestBody String mqMessage,
-                                                @PathVariable("destinationName") String destinationName) throws JsonProcessingException {
+    public ResponseEntity<String> postMQMessage(@RequestBody String mqTextMessage,
+                                                @PathVariable("destinationName") String destinationName,
+                                                @RequestParam("encoding") Optional<String> encoding)
+            throws UnsupportedEncodingException {
 
-        jmsTemplate.convertAndSend(destinationName, mqMessage);
+        //setMessageConverter(encoding);
+        if(encoding.isPresent()) {
+            byte[] mqPayload = mqTextMessage.getBytes(encoding.get());
+            jmsTemplate.convertAndSend(destinationName, mqPayload);
+        }
+        else{
+            jmsTemplate.convertAndSend(destinationName, mqTextMessage);
+        }
+
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    private void setMessageConverter(@RequestParam("encoding") Optional<String> encoding) {
+        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+        messageConverter.setTargetType(MessageType.TEXT);
+        messageConverter.setEncoding("UTF-8");
+
+        if(encoding.isPresent()){
+            messageConverter.setTargetType(MessageType.BYTES);
+            messageConverter.setEncoding(encoding.get());
+        }
+
+        jmsTemplate.setMessageConverter(messageConverter);
     }
 }
